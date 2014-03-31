c19598382023a40add2037ff5b5f567078a31e16
Ali Houshmand <ali.houshmand06@gmail.com>
2014-03-31 11:28:11 -0400

    Create FunApp
    First project at iOS TIY

---
+++ FunApp/FunApp/AHFViewController.h
@@ -0,0 +1,13 @@
+//
+//  AHFViewController.h
+//  FunApp
+//
+//  Created by Ali Houshmand on 3/31/14.
+//  Copyright (c) 2014 Ali Houshmand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AHFViewController : UIViewController
+
+@end
---
+++ FunApp/FunApp/AHFViewController.m
@@ -0,0 +1,29 @@
+//
+//  AHFViewController.m
+//  FunApp
+//
+//  Created by Ali Houshmand on 3/31/14.
+//  Copyright (c) 2014 Ali Houshmand. All rights reserved.
+//
+
+#import "AHFViewController.h"
+
+@interface AHFViewController ()
+
+@end
+
+@implementation AHFViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
