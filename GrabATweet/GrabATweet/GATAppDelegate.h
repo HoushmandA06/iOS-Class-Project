c8c25b71d8e407dc1bd923ef37b36784b880a6fd
Ali Houshmand <ali.houshmand06@gmail.com>
2014-04-13 14:48:45 -0400

    GrabATweet start

---
+++ GrabATweet/GrabATweet/GATAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GATAppDelegate.h
+//  GrabATweet
+//
+//  Created by Ali Houshmand on 4/13/14.
+//  Copyright (c) 2014 Ali Houshmand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GATAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GrabATweet/GrabATweet/GATTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  GATTableViewController.h
+//  GrabATweet
+//
+//  Created by Ali Houshmand on 4/13/14.
+//  Copyright (c) 2014 Ali Houshmand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GATTableViewController : UITableViewController
+
+@end
