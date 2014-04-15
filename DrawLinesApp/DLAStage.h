cae892384c5be20d99c38ea37934db7035407b0e
Ali Houshmand <ali.houshmand06@gmail.com>
2014-04-15 09:34:54 -0400

    DrawLines - added position tracker for touch events

---
+++ DrawLinesApp/DLAStage.h
@@ -0,0 +1,13 @@
+//
+//  DLAStage.h
+//  DrawLinesApp
+//
+//  Created by Ali Houshmand on 4/15/14.
+//  Copyright (c) 2014 Ali Houshmand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLAStage : UIView
+
+@end
---
+++ DrawLinesApp/DLAViewController.h
@@ -0,0 +1,13 @@
+//
+//  DLAViewController.h
+//  DrawLinesApp
+//
+//  Created by Ali Houshmand on 4/15/14.
+//  Copyright (c) 2014 Ali Houshmand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLAViewController : UIViewController
+
+@end
