698b91a544f0815fc5643c60dddc1021274aef45
Ali Houshmand <ali.houshmand06@gmail.com>
2014-05-06 11:01:43 -0400

    added splash

--- PixelNoise/PNAWorldTwoViewController.m
+++ PixelNoise/PNAWorldTwoViewController.m
@@ -15,10 +15,21 @@
 @property (nonatomic) UIGravityBehavior * gravity;
 @property (nonatomic) UICollisionBehavior * collision;
 
+//shards
+@property (nonatomic) UIDynamicItemBehavior * shardBehavior;
+@property (nonatomic) UICollisionBehavior * shardCollision;
+
 
 @end
 
 @implementation PNAWorldTwoViewController
+{
+    PNAPixelSounds * sounds;
+    
+    NSArray * splatterDirections;
+    
+}
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -35,6 +46,25 @@
         [self.animator addBehavior:self.collision];  // all behaviors need to be added to animator
         self.collision.collisionDelegate = self;
         
+        self.shardBehavior = [[UIDynamicItemBehavior alloc] init];
+        self.shardBehavior.density = 10;
+        [self.animator addBehavior:self.shardBehavior];
+        
+    
+        self.shardCollision = [[UICollisionBehavior alloc] init];
+        self.shardCollision.translatesReferenceBoundsIntoBoundary = YES;
+        [self.animator addBehavior:self.shardCollision];  // all behaviors need to be added to animator
+        self.shardCollision.collisionDelegate = self;
+        
+        splatterDirections = @[
+                               [NSValue valueWithCGPoint:CGPointMake(-1.0, -1.0)],
+                               [NSValue valueWithCGPoint:CGPointMake(-0.5, -1.0)],
+                               [NSValue valueWithCGPoint:CGPointMake(0.0, -1.0)],
+                               [NSValue valueWithCGPoint:CGPointMake(0.5, -1.0)],
+                               [NSValue valueWithCGPoint:CGPointMake(1.0, -1.0)]
+                               ];
+        
+        sounds = [[PNAPixelSounds alloc] init];
     
         
      }
@@ -63,15 +93,66 @@
 
 -(void)createBlockWithLocation:(CGPoint)location
 {
-    
     UIView * block = [[UIView alloc] initWithFrame:CGRectMake(location.x,location.y, 10, 10)];
     block.backgroundColor = [UIColor blueColor];
+    block.layer.cornerRadius = 2;
     [self.view addSubview:block];
     [self.gravity addItem:block];
     [self.collision addItem:block];
-    
 }
 
+-(void)collisionBehavior:(UICollisionBehavior *)behavior beganContactForItem:(id<UIDynamicItem>)item withBoundaryIdentifier:(id<NSCopying>)identifier atPoint:(CGPoint)p
+{
+    if([behavior isEqual:self.collision])
+    {
+        NSLog(@"%@", identifier);
+        
+        [sounds playSoundWithName:@"drip"];
+        [self createShardsWithLocation:p];
+        
+        UIView * collidedblock = (UIView *)item; // now item can work with UIView which we will need to remove item from superview
+        
+        [self.gravity removeItem:collidedblock];
+        [self.collision removeItem:collidedblock];
+        [collidedblock removeFromSuperview];
+        
+    }
+    
+if([behavior isEqual:self.shardCollision])
+{
+    UIView * collidedShard = (UIView *)item;
+    [self.gravity removeItem:collidedShard];
+    [self.shardBehavior removeItem:collidedShard];
+    [self.shardCollision removeItem:collidedShard];
+    
+    [collidedShard removeFromSuperview];
+}
+}
+
+
+-(void)createShardsWithLocation:(CGPoint)location
+{
+    for(NSValue * pointValue in splatterDirections) {
+        CGPoint direction = [pointValue CGPointValue];
+       
+        UIView * shard = [[UIView alloc] initWithFrame:CGRectMake(location.x + (direction.x * 10),location.y-50, 8, 8)];
+        shard.backgroundColor = [UIColor orangeColor];
+        shard.layer.cornerRadius = 6;
+        [self.view addSubview:shard];
+        [self.gravity addItem:shard];
+        [self.shardCollision addItem:shard];
+        [self.shardBehavior addItem:shard];
+        
+        UIPushBehavior * pusher = [[UIPushBehavior alloc] initWithItems:@[shard] mode:UIPushBehaviorModeContinuous];
+
+//        [pusher addItem:shard]; dont need this now since initWithitems
+  
+        [self.animator addBehavior:pusher];
+        pusher.pushDirection = CGVectorMake(direction.x, direction.y);
+        
+    }
+    
+}
 
 
 
