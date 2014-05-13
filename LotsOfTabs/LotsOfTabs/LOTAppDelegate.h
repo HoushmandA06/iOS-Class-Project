c2ed742a38eaa80af6a463467fcf10af53f3c20c
Ali Houshmand <ali.houshmand06@gmail.com>
2014-05-13 09:45:09 -0400

    LotsOfTabs tabbing exercise

---
+++ LotsOfTabs/LotsOfTabs/LOTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LOTAppDelegate.h
+//  LotsOfTabs
+//
+//  Created by Ali Houshmand on 5/13/14.
+//  Copyright (c) 2014 Ali Houshmand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LOTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
