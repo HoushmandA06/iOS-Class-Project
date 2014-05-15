a6606bafef6bb66416ed15373f65bd7a17f7a116
Ali Houshmand <ali.houshmand06@gmail.com>
2014-05-15 15:14:39 -0400

    added fireball, explosion, collisions

--- Stuffed Animal Battle/Stuffed Animal Battle/SABGameScene.m
+++ Stuffed Animal Battle/Stuffed Animal Battle/SABGameScene.m
@@ -8,6 +8,19 @@
 
 #import "SABGameScene.h"
 
+////////
+// HP for players
+// HP bars represent HP pool
+// Make fireball remove HP points
+// Sound Effects (w/in spritekit) on explosion, landing after jumping
+// Research Sprite Characters, how designed
+// Find random pics online for your sprite characters (PNG)
+////////
+
+@interface SABGameScene () <SKPhysicsContactDelegate>
+
+@end
+
 @implementation SABGameScene
 {
     SKLabelNode * timerLabel;
@@ -21,7 +34,11 @@
     SKSpriteNode * dpadDown;
     SKSpriteNode * dpadLeft;
     SKSpriteNode * dpadRight;
+    
+    SKSpriteNode * dpadJumpBack;
+    
     SKSpriteNode * player1;
+    SKSpriteNode * player2;
 }
 
 
@@ -32,7 +49,23 @@
     if(self)
         
     {
-        SKSpriteNode * floor = [SKSpriteNode spriteNodeWithColor:[SKColor yellowColor] size:CGSizeMake(SCREEN_WIDTH, 30)];
+       
+        SKPhysicsBody * scenePhysics = [SKPhysicsBody bodyWithEdgeLoopFromRect:CGRectMake(0, 0, size.width, size.height)];
+        
+        self.physicsBody = scenePhysics;
+    
+        self.physicsWorld.contactDelegate = self;
+        
+        SKEffectNode *container = [SKEffectNode node];
+        [self addChild:container];
+        
+//        SKShapeNode *shape = [SKShapeNode node];
+//        shape.path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(20, 20, 50, 50) cornerRadius:12].CGPath;
+//        shape.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+//        shape.strokeColor = [SKColor redColor];
+//        [container addChild:shape];
+
+        SKSpriteNode * floor = [SKSpriteNode spriteNodeWithColor:[SKColor darkGrayColor] size:CGSizeMake(SCREEN_WIDTH, 30)];
         floor.position = CGPointMake(SCREEN_WIDTH/2.0,15);
         [self addChild:floor];
         
@@ -40,7 +73,6 @@
         floorPhysics.affectedByGravity = NO;
         floorPhysics.dynamic = NO;
         floor.physicsBody = floorPhysics;
-        
         
         timerLabel = [SKLabelNode node];
         timerLabel.position = CGPointMake(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT - 20);
@@ -71,7 +103,7 @@
         [self addChild:dpadDown];
         
         dpadUp = [SKSpriteNode spriteNodeWithColor:[SKColor blueColor] size:CGSizeMake(30, 30)];
-        dpadUp.position = CGPointMake(80, 120);
+        dpadUp.position = CGPointMake(100, 120);
         [self addChild:dpadUp];
         
         dpadLeft = [SKSpriteNode spriteNodeWithColor:[SKColor blueColor] size:CGSizeMake(30, 30)];
@@ -82,26 +114,78 @@
         dpadRight.position = CGPointMake(120, 80);
         [self addChild:dpadRight];
         
+        dpadJumpBack = [SKSpriteNode spriteNodeWithColor:[SKColor lightGrayColor] size:CGSizeMake(30, 30)];
+        dpadJumpBack.position = CGPointMake(60,120);
+        [self addChild:dpadJumpBack];
+        
         player1 = [SKSpriteNode spriteNodeWithColor:[SKColor whiteColor] size:CGSizeMake(40, 100)];
-        player1.position = CGPointMake(SCREEN_WIDTH/2.0, 80);
+        player1.position = CGPointMake(SCREEN_WIDTH/2.0 - 250, 80);
         [self addChild:player1];
-        
         SKPhysicsBody * playerPhysics = [SKPhysicsBody bodyWithRectangleOfSize:player1.size];
-        
         player1.physicsBody = playerPhysics;
         
-      
-        
 
+        player2 = [SKSpriteNode spriteNodeWithColor:[SKColor cyanColor] size:CGSizeMake(40, 100)];
+        player2.position = CGPointMake(SCREEN_WIDTH/2.0 + 250,80);
+        [self addChild:player2];
+        SKPhysicsBody * player2Physics = [SKPhysicsBody bodyWithRectangleOfSize:player2.size];
+        player2.physicsBody = player2Physics;
+        
+        player2.userData = [@{@"type":@"player2"} mutableCopy];
+        
+        player2.physicsBody.contactTestBitMask = 1; //listening for collisions of the same category
+        
 
     }
     return self;
+}
+
+
+-(void)update:(NSTimeInterval)currentTime
+{
+    
     
 }
 
+
+-(void)didBeginContact:(SKPhysicsContact *)contact
+{
+    
+    
+    NSMutableArray * nodes = [@[] mutableCopy];
+    
+    if(contact.bodyA.node != nil) [nodes addObject:contact.bodyA.node];
+    if(contact.bodyB.node != nil) [nodes addObject:contact.bodyB.node];
+     
+    
+//NSArray * nodes = @[contact.bodyA.node,contact.bodyB.node];
+
+    for(SKNode * node in nodes)
+    {
+     if([node.userData[@"type"] isEqualToString:@"fireball"])
+     {
+         
+         
+         [node removeFromParent];
+         
+         NSString * myParticlePath = [[NSBundle mainBundle] pathForResource:@"explosion" ofType:@"sks"];
+         
+         SKEmitterNode * explosion = [NSKeyedUnarchiver unarchiveObjectWithFile:myParticlePath];
+         
+         explosion.position = contact.contactPoint;
+         
+         explosion.numParticlesToEmit = 200;
+         
+         [self addChild:explosion];
+         
+     }
+    }
+}
+
+
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    UITouch * touch = [touches anyObject];
+   // UITouch * touch = [touches anyObject];
     
     CGPoint location = [[touches anyObject] locationInNode:self];
 
@@ -110,7 +194,7 @@
 
 -(void)testButtonsWithLocation:(CGPoint)location
 {
-    NSArray * buttons = @[buttonA, buttonB, dpadUp,dpadDown,dpadLeft,dpadRight];
+    NSArray * buttons = @[buttonA, buttonB, dpadUp,dpadDown,dpadLeft,dpadRight,dpadJumpBack];
     for(SKNode * button in buttons) {
         
         if([button containsPoint:location])
@@ -120,7 +204,31 @@
             switch ([buttons indexOfObject:button])
             {
                 case 0: // buttonA
-                    NSLog(@"Punch");
+                {   NSLog(@"Fire");
+                    
+//                    SKSpriteNode * fireball = [SKSpriteNode spriteNodeWithColor:[SKColor redColor] size:CGSizeMake(10,10)];
+                   
+                    
+                    NSString * myParticlePath = [[NSBundle mainBundle] pathForResource:@"blueflame" ofType:@"sks"];
+                    
+                    SKEmitterNode * fireball = [NSKeyedUnarchiver unarchiveObjectWithFile:myParticlePath];
+                    
+                    fireball.position = CGPointMake(player1.position.x + 26, player1.position.y);
+                    
+                    SKPhysicsBody * fireballPhysics = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(10, 10)];
+                    
+                    fireball.physicsBody = fireballPhysics;
+                    
+                    fireball.physicsBody.contactTestBitMask = 1;  //fireball will register on player 2 physicsbody
+                    
+                    fireballPhysics.affectedByGravity = NO;
+                    
+                    fireball.userData = [@{@"type":@"fireball"} mutableCopy];
+                    
+                    [self addChild:fireball];
+                    [fireballPhysics applyImpulse:CGVectorMake(6.0, 0.0)];
+
+                }
                     break;
                     
                 case 1: // buttonB
@@ -155,6 +263,11 @@
                    NSLog(@"Right");
                     [player1.physicsBody applyImpulse:CGVectorMake(20.0, 0.0)];
                     break;
+                    
+                case 6: // dpadJumpBack
+                    NSLog(@"Jumped Back");
+                    [player1.physicsBody applyImpulse:CGVectorMake(-5, 100)];
+                    
                     
                 default:break;
                     
